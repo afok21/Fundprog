e92d5a59142da01199343f926b93c870bf3a78ea
leoalves81 <leonardoalvesdavinci@gmail.com>
2025-05-07 14:54:38 +0100

    Add files via upload

---
+++ maincubo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  7 14:53:36 2025
+
+@author: ALFIBRAGA
+"""
+
+from cubo import Cubo
+from graphics import *
+
+def main():
+    cubo1 = Cubo(10)
+    cubo1.faceArea()
+    cubo1.surfaceArea()
+    
+main()
